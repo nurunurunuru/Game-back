4c2d093cdbb3c719a3b28371060b0d80202367f6
nurunurunuru <nursajid1200@gmail.com>
2026-03-08 03:41:15 +0600

    commit message

--- server.js
+++ server.js
@@ -1,39 +1,53 @@
-import express from "express";
-import cors from "cors";
-import 'dotenv/config';
-import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
-import userRouter from "./routes/userRoute.js";
-import productRouter from "./routes/productRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js"
+import productRouter from "./routes/productRoute.js"
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
-//app config
-const app = express();
-const port = process.env.PORT || 4000;
+// app config
+const app = express()
+const port = process.env.PORT || 4000
 
-connectDB();
-connectCloudinary();
+connectDB()
+connectCloudinary()
 
-//middleware
+// allowed origins
+const allowedOrigins = [
+  
+  "https://game-admin-beta.vercel.app",
+  "http://localhost:5173"
+]
 
-// Allow only your frontend domain
-app.use(cors({
-    origin: 'https://game-liard-sigma.vercel.app',  // <-- your frontend URL
-    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-    credentials: true   // allow cookies if needed
-}));
+// middleware
+app.use(express.json())
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true
+  })
+)
 
-//api end points
-app.use('/api/user', userRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+// api endpoints
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
-app.get('/',(req,res)=>{
-    res.send("Api Working");
-});
+app.get('/', (req, res) => {
+  res.send("Api Working")
+})
 
-app.listen(port, ()=> console.log('Server is running on PORT : '+ port));
+app.listen(port, () => console.log('Server is running on PORT : ' + port))
+
+export default app
